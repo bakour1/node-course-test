3c5767f64295f1c4c2cd4f2a4dcf4fd93317045d
bakour1 <bakour512@gmail.com>
2023-03-03 20:43:21 +0100

    Redirecting Requests

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res, next) => {
   const url = req.url;
@@ -10,6 +11,13 @@ const server = http.createServer((req, res, next) => {
       '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
     );
     res.write('</html>');
+    return res.end();
+  }
+
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
     return res.end();
   }
 
