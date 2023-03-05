86135d5a6587d251c546301f2bab24656481b733
bakour1 <bakour512@gmail.com>
2023-03-05 20:56:19 +0100

    67. Adding a 404 Error Page
    الآن أحد الأشياء التي فعلناها في المحاضرات الأخيرة هو أننا استخدمنا جهاز التوجيه السريع والآن
    
    ، لدينا إعداد حيث لدينا أيضًا بعض المسارات غير المعالجة. إذا أدخلنا سلسلة عشوائية هنا ، فسنحصل على هذا الخطأ.
    
    الآن عادةً ، نود أن نرى صفحة خطأ 404 ويمكننا القيام بذلك. مرة أخرى في التطبيق. js ، يمكننا الاستفادة من
    
    البرامج الوسيطة أو استخدام way express للأدوات الوسيطة وتوجيه
    
    الطلب من خلالها.
    
    تذكر أن الطلب ينتقل من أعلى إلى أسفل ، لذا إذا عثر على
    
    بعض البرامج الوسيطة التي تتعامل معه ، فسيتم إدخاله هناك ثم على سبيل المثال هنا للشرطة المائلة مع طريقة
    
    get على جهاز التوجيه ، سننتهي هنا بالفعل
    
    ، ولن نحصل أيضًا على التالي اتصل هنا
    
    حتى لا يتم تنفيذ أي برمجيات وسيطة أخرى. ولكن إذا لم يكن لدينا برمجيات وسيطة مناسبة ولم يكن لدينا
    
    واحدة هنا ، فإننا في الواقع نجعلها تصل إلى الأسفل وفي النهاية لا نتعامل مع هذا الطلب.
    
    لذلك لإرسال صفحة خطأ 404 ، علينا ببساطة إضافة التقاط جميع البرامج الوسيطة في الجزء السفلي حيث لا نحتاج
    
    إلى مرشح مسار ولكن يمكننا إضافة شرطة مائلة ولكن هذا هو الإعداد
    
    الافتراضي على أي حال ثم التعامل مع استجابة
    
    الطلب بعد ذلك لأنني أستخدم هنا ، سيتعامل هذا أيضًا مع جميع طرق http ، وليس مجرد الحصول على الطلبات وهناك
    
    ، يمكنني أيضًا إرسال بعض الرموز مثل لم يتم العثور على الصفحة.
    
    وثيقة html صغيرة جدًا وربما نرغب أيضًا في تعيين رمز الحالة 404 ويمكنك القيام بذلك عن طريق تسلسل طريقة
    
    أخرى قبل الإرسال وهذه هي طريقة الحالة وبالطبع لا يمكنك استخدام ذلك هنا ، كان
    
    من الممكن أن تستخدمه هنا أو
    
    في المشرف. js أيضًا.
    
    لذلك دائمًا قبل الإرسال ، يمكنك استدعاء الحالة أو تعيين الرأس ، يمكنك
    
    بالفعل ربط جميع استدعاءات الطريقة هذه ، ويجب أن يكون الإرسال هو الأخير.
    
    لذلك أنا هنا أتصل بالحالة لتعيين رمز الحالة الخاص بي ، وهي طريقة ملائمة أخرى تمت إضافتها بواسطة expressjs وسأقوم
    
    بتعيينها على 404 وهو الرمز الشائع لصفحة لم يتم العثور عليه. مع إضافة هذا ، إذا قمت بحفظ هذا ثم أعدت الآن
    
    تحميل المسار الوهمي الخاص بي هنا ، لم يتم العثور على الصفحة بينما بالنسبة لإضافة منتج ، ما زلت أحصل على هذا النموذج ، لذلك
    
    لا يزال هذا
    
    يعمل ، ومع ذلك تغير
    
    الباقي.

--- app.js
+++ app.js
@@ -11,4 +11,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+// handle 404 page
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page Not Found!</h1>');
+});
+
 app.listen(3000);
