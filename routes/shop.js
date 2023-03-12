61abbef512b31df7a60a2c3cbdee64a7398319a2
bakour1 <bakour512@gmail.com>
2023-03-12 15:20:15 +0100

    132. Fixing a Delete Product Bug
    الآن أنا سعيد جدًا بحالة هذا التطبيق
    
    ولكن هناك مشكلة بسيطة فيه.
    
    إذا لم يكن لدينا أي منتجات في عربة التسوق أو على الأقل ليس المنتج الذي نحاول
    
    حذفه ، فلنفترض أن المنتج
    
    الموجود على اليمين هنا
    
    ، حصلنا على خطأ بالفعل ونحصل على خطأ لأننا نحاول الوصول إلى كمية منتج ليس لدينا فقط.
    
    والسبب في ذلك هو أننا ببساطة نستخدم في منتجاتنا. js في مجلد النماذج ، نستخدم نموذج
    
    العربة لحذف المنتج من هناك ،
    
    صحيح.
    
    من الواضح أن المشكلة ليست كل منتج في سلة التسوق ، لذلك في العربة
    
    هنا حيث قمنا بحذف منتج ، نحتاج أولاً وقبل كل شيء إلى التحقق مما إذا كان منتج معين جزءًا من سلة
    
    التسوق.
    
    لذلك هنا عندما أقوم بتحليل سلة التسوق الإجمالية ، هنا عندما أجد المنتج في سلة التسوق ، يجب أن أتحقق مما
    
    إذا كان لدينا هذا المنتج حقًا لأنه إذا لم يكن لدينا ، لذلك إذا
    
    أضفت علامة تعجب أمام هذا ، لذلك إذا كان
    
    هذا التحقق هنا صحيحًا ، فهذا يعني أنه ليس لدينا منتج ، فأنا ببساطة بحاجة للعودة إلى هنا ، ولا أريد
    
    المتابعة ، ولا أريد محاولة
    
    تحريره لأنه ليس جزءًا من سلة التسوق. لذلك الآن إذا قمت بإعادة تحميل تطبيقي
    
    وأضفت بسرعة منتجًا جديدًا هنا ، عفوًا ، بسعر ووصف مثل هذا ، ثم قمت بالنقر فوق منتجات
    
    المسؤول وحذف هذا ، هذا يعمل الآن

--- routes/shop.js
+++ routes/shop.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const shopController = require('../controllers/products');
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
